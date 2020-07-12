317905d500a33e452db7b448c0d1965960046db7
Angelo Giron <agiron_1907@hotmail.com>
2020-07-12 17:50:06 -0500

    Updates

---
+++ precache-manifest.818c8b436d9cdfe223b23135f4760122.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f8ab6e9849765e12efacbd120b1393",
+    "url": "/anime-sama-radio/index.html"
+  },
+  {
+    "revision": "97e3d8a323f036987423",
+    "url": "/anime-sama-radio/static/css/main.a74332f0.chunk.css"
+  },
+  {
+    "revision": "73e2a1d04aee577de693",
+    "url": "/anime-sama-radio/static/js/2.ae98d196.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/anime-sama-radio/static/js/2.ae98d196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e3d8a323f036987423",
+    "url": "/anime-sama-radio/static/js/main.079c980d.chunk.js"
+  },
+  {
+    "revision": "9808cb0c353b8686a528",
+    "url": "/anime-sama-radio/static/js/runtime-main.c30dee7d.js"
+  },
+  {
+    "revision": "829d2cbfafe994ba186b97f8ea0d4b4c",
+    "url": "/anime-sama-radio/static/media/disk.829d2cbf.png"
+  },
+  {
+    "revision": "02e7940cbff63de74bb261800a859dce",
+    "url": "/anime-sama-radio/static/media/googleIcon.02e7940c.png"
+  }
+]);
